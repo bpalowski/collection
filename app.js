7fb2b5d04ebfe67a7426edecee24b31b7c375e06
b.palowski <brianpalowski00@gmail.com>
2019-01-08 00:32:33 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// document.get;
+
+// alert("hello");
+// let ar = ["lets", "practice", "working", "with", "algorithms"];
+
+// function returnStr(arr, num) {
+//   let newArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length >= num) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// document.getElementById("first-function").innerHTML = returnStr(ar, 5);
